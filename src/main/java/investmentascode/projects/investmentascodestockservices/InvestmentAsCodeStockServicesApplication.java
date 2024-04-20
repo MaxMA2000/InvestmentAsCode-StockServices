abb3c1a230eeb13e8a5a3a168929f5fec4a0f67e
MaxMA2000 <maxmahklife@gmail.com>
2024-04-20 15:14:53 +0800

    setup(StockServices): add the project setup

---
+++ src/main/java/investmentascode/projects/investmentascodestockservices/InvestmentAsCodeStockServicesApplication.java
@@ -0,0 +1,13 @@
+package investmentascode.projects.investmentascodestockservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentAsCodeStockServicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvestmentAsCodeStockServicesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/investmentascode/projects/investmentascodestockservices/InvestmentAsCodeStockServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package investmentascode.projects.investmentascodestockservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAsCodeStockServicesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
